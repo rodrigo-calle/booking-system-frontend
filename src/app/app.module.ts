07745ca55e01b43bd5fb561f76fd1734b5af5a67
rodrigo-calle <rodrigoc_0@hotmail.com>
2022-04-09 18:40:53 -0500

    some change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
