0c425c806951d230ff323a9589613f35a45f8666
WonderWoman <dayanne1429@gmail.com>
2022-04-10 02:37:10 -0500

    CambiosDaya

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { AttractionsComponent } from "./pages/attractions/attractions.component"
 import { HomeComponent } from "./pages/home/home.component";
 import { ReservaComponent } from "./pages/reserva/reserva.component";
 import { RegisterComponent } from "./pages/register/register.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { ListadohabitacionesComponent } from "./pages/listadohabitaciones/listadohabitaciones.component";
 
 
 const routes:Routes = [
     {path:'register',component:RegisterComponent},
     {path:'reserva',component:ReservaComponent},
     { path: '', component: HomeComponent },
-    { path: 'attractions', component: AttractionsComponent }
+    { path: 'attractions', component: AttractionsComponent },
+    { path: 'login', component:LoginComponent},
+    { path: 'listadohabitaciones', component:ListadohabitacionesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AttractionsComponent } from './pages/attractions/attractions.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ListadohabitacionesComponent } from './pages/listadohabitaciones/listadohabitaciones.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { AttractionsComponent } from './pages/attractions/attractions.component'
     RegisterComponent,
     HomeComponent,
     AttractionsComponent,
+    LoginComponent,
+    ListadohabitacionesComponent,
 
   ],
   imports: [
