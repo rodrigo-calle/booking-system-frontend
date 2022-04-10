f8b89f10a2ba3b915a78976b3c75a9b080e40beb
rodrigo-calle <rodrigoc_0@hotmail.com>
2022-04-09 20:08:40 -0500

    adding attraction page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AttractionsComponent } from "./pages/attractions/attractions.component";
 import { HomeComponent } from "./pages/home/home.component";
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'attractions', component: AttractionsComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AttractionsComponent } from './pages/attractions/attractions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AttractionsComponent,
   ],
   imports: [
     BrowserModule,
