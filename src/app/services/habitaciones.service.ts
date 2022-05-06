183b393c1e46d98bafcbf84bc188dd2e43e8ca23
Sebastian <sebastian.montano@seidor.com>
2022-05-05 23:52:26 -0500

    hasta donde esta

--- src/app/services/habitaciones.service.ts
+++ src/app/services/habitaciones.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import habitaciones from 'src/assets/json/habitaciones.json';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HabitacionesService {
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
 
   _getHabitaciones() {
-    return habitaciones.data;
+    return this.http.get("https://localhost:44309/api/habitacion/listar");
+  }
+
+  _getHabitacionesAgrup() : Observable<any>{
+    return this.http.get("https://localhost:44309/api/habitacion/listar_agrup");
+  }
+
+  _insertReserva(data :any){
+    return this.http.post<any>('https://localhost:44309/api/reserva/insert', data);
   }
 
 }
